7e146e3ab528cbddbc6034c763506c2607e0a22e
Amuk kumar <amukkumar@Amuks-MacBook-Air.local>
2024-02-14 19:10:03 +0530

    first commit

--- src/components/experience.jsx
+++ src/components/experience.jsx
@@ -6,7 +6,7 @@ const Experience = () => {
     return (
         <div className='relative'>
             <div className='experience flex flex-col gap-2'>
-                <div className='flex gap-3 md:justify-end mt-5'>
+                <div className='flex md:flex-row flex-col gap-3 md:justify-end md:items-start items-center mt-5'>
                     <p className='mt-7 hidden md:block plain text-xl'>20-0ct-2022</p>
                     <Image src={react} className='h-20 w-20 rounded-full ex-border p-2 bg-white '></Image>
                     <div className='shadow left-card p-5 rounded-lg'>
@@ -26,7 +26,7 @@ const Experience = () => {
                     </div>
 
                 </div>
-                <div className='flex gap-3 md:justify-end md:flex-row-reverse mt-5'>
+                <div className='flex gap-3 md:justify-end md:flex-row-reverse flex-col md:justify-end md:items-start items-center mt-5'>
                     <p className='mt-7 hidden md:block plain text-xl'>20-0ct-2022</p>
                     <Image src={react} className='h-20 w-20 rounded-full ex-border p-2 bg-white '></Image>
                     <div className='shadow left-card p-5 rounded-lg'>
