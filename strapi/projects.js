ca4cc74a08c14b625dfaacfd9d4dbbd7a7125ef3
Amuk kumar <amukkumar@Amuks-MacBook-Air.local>
2024-02-25 23:30:17 +0530

    neew

--- strapi/projects.js
+++ strapi/projects.js
@@ -2,7 +2,7 @@ import axios from 'axios';
 
 export const fetchProjects = async () => {
     try {
-        const response = await axios.get('http://localhost:1337/api/projects?populate=*');
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/projects?populate=*`);
         return response.data;
     } catch (error) {
         console.error('Error fetching projects:', error);
@@ -12,7 +12,7 @@ export const fetchProjects = async () => {
 
 export const fetchProjectById = async (projectId) => {
     try {
-        const response = await axios.get(`http://localhost:1337/api/projects/${projectId}?populate=*`);
+        const response = await axios.get(`${process.env.NEXT_PUBLIC_API_URL}/api/projects/${projectId}?populate=*`);
         return response.data;
     } catch (error) {
         console.error(`Error fetching project with ID ${projectId}:`, error);
